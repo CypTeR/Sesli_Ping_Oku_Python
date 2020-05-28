72132fc494ed171de1a96c78249b08507982949a
Mikail Aksu <oyuns663@gmail.com>
2020-05-28 18:08:08 +0300

    Python3 file

--- PingSonucOku.py
+++ PingSonucOku.py
@@ -12,10 +12,11 @@ try:
     okut = host+" pinginiz ortalama "+strcvr+"ms" #birleştirme
     tts = gTTS(text=okut, lang='tr') #yazıyı sese çevir
     print(cevir)
-    tts.save("merhaba.mp3") #sesi kaydet
-    os.system("merhaba.mp3") #sesi oynat
-    os.system("del merhaba.mp3") #sesi sil
+    tts.save("pingsesi.mp3") #sesi kaydet
+    os.system("pingsesi.mp3") #sesi oynat
+    os.system("del pingsesi.mp3") #sesi sil
 
 
 except:
     print("Boş veya Geçersiz Host!")
+    os.system("python PingSonucOku.py")
