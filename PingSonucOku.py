a4cd8480008356f841fd09b2a269240982867fc4
Mikail Aksu <oyuns663@gmail.com>
2020-05-28 17:54:38 +0300

    Update PingSonucOku.py

--- PingSonucOku.py
+++ PingSonucOku.py
@@ -3,15 +3,19 @@ from gtts import gTTS #gtts kütüphanesi "pip install gtts"
 from pythonping import ping #ping kütühanesi "pip install pythonping"
 
 print("Host Giriniz: ")
-host = input() #host girişi
 
-sonuc = ping(host)
-cevir = int(sonuc.rtt_avg_ms)
-strcvr = str(cevir)
-okut = host+" pinginiz ortalama "+strcvr+"ms" #birleştirme
-tts = gTTS(text=okut, lang='tr') #yazıyı sese çevir
-print(cevir)
+try:
+    host = input() #host girişi
+    sonuc = ping(host)
+    cevir = int(sonuc.rtt_avg_ms)
+    strcvr = str(cevir)
+    okut = host+" pinginiz ortalama "+strcvr+"ms" #birleştirme
+    tts = gTTS(text=okut, lang='tr') #yazıyı sese çevir
+    print(cevir)
+    tts.save("merhaba.mp3") #sesi kaydet
+    os.system("merhaba.mp3") #sesi oynat
+    os.system("del merhaba.mp3") #sesi sil
 
-tts.save("merhaba.mp3") #sesi kaydet
-os.system("merhaba.mp3") #sesi oynat
-os.system("del merhaba.mp3") #sesi sil
+
+except:
+    print("Boş veya Geçersiz Host!")
